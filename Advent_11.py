ebb35d07f4a8490c48e7012243bceb280aa8b6c1
AWebZen <30926788+AWebZen@users.noreply.github.com>
2019-02-13 15:16:16 +0100

    Cleaned up a little

--- Advent_11.py
+++ Advent_11.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-serial = 5468
 
-def power_fuel(serial, coords = False):
+def power_fuel(serial, coords=False):
+	"""
+	Evaluate power level of each fuel cell.
+    	
+	Parameters
+    -------
+    serial - serial (puzzle input)
+    coords - coordinates. If given, prints power level of given coordinates.
+             Defaults to False.
+             
+    Returns
+    -------
+    power - numpy array of fuel cell power level
+	"""
 	X = np.zeros((300,300))
 	Y = np.zeros((300,300))
 	for i in range(1,300+1):
@@ -12,40 +24,74 @@ def power_fuel(serial, coords = False):
 	power = np.zeros((300,300))
 	for i in range(300):
 		for j in range(300):
-			power[i,j] = int(str(int((rackID[i,j] * Y[i,j] + serial) * rackID[i,j]))[-3]) - 5
+			try:
+				power[i,j] = int(str(int((rackID[i,j] * Y[i,j] + serial) 
+                                 * rackID[i,j]))[-3]) - 5
+			except IndexError:
+				power[i,j] = -5
 	if coords:
 		print power[coords[0]-1, coords[1]-1]
 	return power
 
-power = power_fuel(57, [122,79])
-power = power_fuel(39, [217,196])
-power = power_fuel(71, [101,153])
-power = power_fuel(18, [33,45])
 
-power = power_fuel(5468)
 
 def largest_power(power, size):
-	power_3x3 = np.zeros((300-size+1,300-size+1))
+    """
+    Finds the size x size square with the largest total power. It is identified by the 
+    top-left fuel cell.
+    
+    Parameters
+    -------
+    power - numpy array of fuel cell power level
+    size - int, size of square
+    
+    Returns
+    -------
+    Top-left fuel cell coordinates of largest total power size x size square.
+    Total power associated to square.
+    """
+	power_3x3 = np.zeros((300-size+1, 300-size+1))
 	for i in range(300-size+1):
 		for j in range(300-size+1):
 			power_3x3[i,j] = np.sum(power[i:i+size,j:j+size])
-	return np.array(np.unravel_index(np.argmax(power_3x3), power_3x3.shape))+1, np.max(power_3x3)
+	return np.array(np.unravel_index(np.argmax(power_3x3), power_3x3.shape)) + 1, np.max(power_3x3)
 
-print largest_power(power, 3)
-		
-#Part 2
+
 
 def largest_power_sizes(power):
+    """
+    Finds identifier and size of the size x size square with largest total power.
+    
+    Parameters
+    -------
+    power - numpy array of fuel cell power level
+    
+    Returns
+    -------
+    Size of size x size square
+    Top-left fuel cell coordinates of largest total power size x size square.
+    Total power associated to square.
+    """
 	maxes = []
 	max_ind = []
 	for size in range(1,301):
-		print size
 		ind, mxx = largest_power(power, size)
 		maxes.append(mxx)
 		max_ind.append(ind)
 	return np.argmax(maxes)+1, max_ind[np.argmax(maxes)], np.max(maxes)
 
+
+# PART 1
+# Tests
+
+#power = power_fuel(57, [122,79])
+#power = power_fuel(39, [217,196])
+#power = power_fuel(71, [101,153])
+#power = power_fuel(18, [33,45])
+
+power = power_fuel(5468) #puzzle input
+print largest_power(power, 3)
+
+# PART 2
 print largest_power_sizes(power)
 	
-
-
--- Advent_13.py
+++ Advent_13.py
@@ -1,121 +1,167 @@
 import numpy as np
 from copy import deepcopy
 
-rails = []
-with open("Advent_13_input.txt", "r") as f:
-	for line in f:
-		line = line.rstrip("\n")
-		rails.append(list(line))
+"""
+^, v, >, < : cart directions
+l, s, r: left, straight, right turns at intersections.
+"""
+    
+def get_rails_wagons(fname="Advent_13_input.txt"):
+    """Get railway and wagons from input"""
+    # Rails with wagons
+    rails = []
+    with open(fname, "r") as f:
+        for line in f:
+            line = line.rstrip("\n")
+            rails.append(list(line))
+    
+    rails = np.array(rails)
 
-rails = np.array(rails)
+    # Wagons definition
+    wagons = {} # {id:[coords, direction, future intersection behaviour]}
+    wag_counter = 0
+    for direction in ["<", ">", "^", "v"]:
+        wags = np.array(np.where(rails == direction)).T
+        for w in wags:
+            wagons[wag_counter] = [list(w), direction, "l"]
+            wag_counter += 1
+    
+    # Removal of wagons from rails
+    no_wags_rails = deepcopy(rails)
+    #On initial map, track under cart is a straight path in the cart direction
+    for w, wag in wagons.items():
+        i,j = wag[0]
+        if wag[1] in [">","<"]:
+            actual_sign = "-"
+        else:
+            actual_sign = "|"
+        no_wags_rails[i,j] = actual_sign
+    return no_wags_rails, wagons
 
 
-intersection = {"l":"s", "s":"r", "r":"l"}
+def get_new_coords (i, j, dirc):
+    """New coordinates depending on cart direction"""
+    return {"<":[i,j-1],
+            ">":[i,j+1], 
+            "^":[i-1,j], 
+            "v":[i+1,j],
+            }[dirc]
 
-new_dir_intersection = {"l":{"<":"v", ">":"^", "^":"<", "v":">"}, 
-	"s":{"<":"<", ">":">", "^":"^", "v":"v"},
-	"r":{"<":"^", ">":"v", "^":">", "v":"<"},
-	}
 
-def get_new_coords (i,j, dirc):
-	return {"<": [i,j-1],
-	">":[i,j+1], 
-	"^":[i-1,j], 
-	"v":[i+1,j],
-	}[dirc]
-
-def get_order_carts(wagons): #loop on carts from top to bottom, left to right
-	coords = []
-	for w in sorted(wagons.keys()):
-		coords.append(list(wagons[w][0])+[w])
-	coords = np.array(coords)
-	new_sort = np.argsort(coords[:,0])
-	return np.array(coords)[new_sort,2]
-	
+def get_order_carts(wagons): 
+    """Carts/wagons move in reading-order:left to right, top to down"""
+    coords = []
+    for w in sorted(wagons.keys()):
+        coords.append(list(wagons[w][0]) + [w])
+    coords = np.array(coords)
+    new_sort = np.argsort(coords[:,0])
+    return np.array(coords)[new_sort,2]
+    
 
 def get_new_direction(dirc, curve):
-	return {"/" : {"^":">", "<":"v",">":"^","v":"<",},
-		"\\" : {"^":"<", "v":">", "<":"^", ">":"v",}
-		}[curve][dirc]
+    """Direction change after curve"""
+    return {"/" : {"^":">", "<":"v",">":"^","v":"<",},
+        "\\" : {"^":"<", "v":">", "<":"^", ">":"v",}
+        }[curve][dirc]
 
 
-def next_step(wag, rails, new_dir_intersection,no_wags_rails):
-	#print wag, rails[wag[0][0],wag[0][1]]
-	i,j = get_new_coords(wag[0][0],wag[0][1], wag[1])
-	next_step = rails[i,j]
-	i_old, j_old = wag[0][0],wag[0][1]
-	wag[0] = np.array([i,j])
-	if next_step == "/" or next_step == "\\":
-		wag[1] = get_new_direction(wag[1], next_step)
-	elif next_step == "+":
-		wag[1] = new_dir_intersection[wag[2]][wag[1]]
-		wag[2] = intersection[wag[2]]
-	elif next_step in ["<", ">", "^", "v"]:
-		rails[i_old,j_old] = no_wags_rails[i_old,j_old]
-		rails[i,j] = no_wags_rails[i,j]
-		return wag[0], rails
-	#print wag, rails[i,j], wag[1]
-	rails[i_old,j_old] = no_wags_rails[i_old,j_old]
-	rails[i,j] = wag[1]
-	return wag, rails
-		
+def next_step(wag, wagons, no_wags_rails):
+    """
+    Our cart takes a step
+    
+    Parameters
+    -------
+    wag - wagon defined by [coords, direction, future intersection behaviour]
+    no_wags_rails - numpy array of the rail grid without carts
+    
+    Returns
+    -------
+    wag - wagon defined by [new coords, new direction, future intersection behaviour]
+    no_wags_rails - numpy array of the rail grid without carts
+    """
+    #Our cart moves...
+    i_old, j_old = wag[0][0], wag[0][1]
+    i, j = get_new_coords(i_old, j_old, wag[1])
+    nxt_step = no_wags_rails[i,j]
+    
+    #What does it find?
+    if [i,j] in [wagn[0] for w, wagn in wagons.items()]: #collision
+        return [i,j], no_wags_rails
+    
+    elif nxt_step == "/" or nxt_step == "\\": #curve
+        wag[1] = get_new_direction(wag[1], nxt_step)
+        
+    elif nxt_step == "+": #intersection
+        wag[1] = NEW_DIR_INTERSECTION[wag[2]][wag[1]]
+        wag[2] = INTERSECTION[wag[2]]
+    
+    #straight path
+    wag[0] = [i,j] #updating coordinates
+    return wag, no_wags_rails
+        
 
-wagons = {}
-wag_counter = 0
-for direction in ["<", ">", "^", "v"]:
-	wags = np.array(np.where(rails == direction)).T
-	for w in wags:
-		wagons[wag_counter] = [w, direction, "l"]
-		wag_counter += 1
-
-
-no_wags_rails = deepcopy(rails)
-for w, wag in wagons.items():
-	i,j = wag[0]
-	if wag[1] in [">","<"]:
-		actual_sign = "-"
-	else:
-		actual_sign = "|"
-	no_wags_rails[i,j] = actual_sign
-
-def part1 (wagons, rails, new_dir_intersection, no_wags_rails):
-	while True:
-		order_carts = get_order_carts(wagons)
-		for w in order_carts:
-			wag = wagons[w]
-			wagons[w], rails = next_step(wag, rails, new_dir_intersection, no_wags_rails)
-			if len(wagons[w]) == 2:
-				break
-		if len(wagons[w]) == 2:
-			print wagons[w][1], wagons[w][0] #Inverted axis in text
-			break
-
-#part1(wagons, rails, new_dir_intersection, no_wags_rails)
+def part1 (wagons, no_wags_rails):
+    """
+    Find coordinates of first collision
+    """
+    while True:
+        order_carts = get_order_carts(wagons)
+        for w in order_carts:
+            wag = wagons[w]
+            wagons[w], no_wags_rails = next_step(wag, wagons, no_wags_rails)
+            if len(wagons[w]) == 2:
+                break
+        if len(wagons[w]) == 2:
+            print "Part 1"
+            print "Location of first crash: {}, {}".format(wagons[w][1], wagons[w][0]) #Inverted axis in text
+            break
 
 
+def part2 (wagons, no_wags_rails):
+    """Where will the last cart that hasn't crashed end up?"""
+    step = 0
+    while len(wagons) > 1:
+        order_carts = get_order_carts(wagons)
+        for w in order_carts:
+            if w not in wagons.keys():
+                continue
+            wag = wagons[w]
+            stop = False
+            if len(wagons)%2 == 0: #Not a single cart alive
+                stop = True
+                break
+            wagons[w], no_wags_rails = next_step(wag, wagons, no_wags_rails)
+            if len(wagons[w]) == 2:
+                coords_remove = wagons[w]
+                del wagons[w]
+                for w, wag in wagons.items():
+                    if wag[0][0] == coords_remove[0] and wag[0][1] == coords_remove[1]:
+                        del wagons[w]
+        if stop:
+            break
+        step += 1
+    print "Part 2"
+    print "Location of last cart: {}, {}".format(wagons.values()[0][0][1], wagons.values()[0][0][0]) #Inverted axis in text
+    
 
-#PART 2
-step = 0
-while len(wagons) > 1:
-	order_carts = get_order_carts(wagons)
-	for w in order_carts:
-		if w not in wagons.keys():
-			continue
-		wag = wagons[w]
-		stop = False
-		if len(wagons)%2 == 0:
-			stop=True
-			break
-		wagons[w], rails = next_step(wag, rails, new_dir_intersection, no_wags_rails)
-		if len(wagons[w]) == 2:
-			coords_remove = wagons[w]
-			del wagons[w]
-			for w, wag in wagons.items():
-				if wag[0][0] == coords_remove[0] and wag[0][1] == coords_remove[1]:
-					del wagons[w]
-	if stop:
-		break
-	step += 1
+#Looping on intersection behaviour
+INTERSECTION = {"l":"s", "s":"r", "r":"l"}
 
-print wagons.values()[0][0][1], wagons.values()[0][0][0]
+#New direction after intersection, depending on behaviour
+NEW_DIR_INTERSECTION = {"l": {"<":"v", ">":"^", "^":"<", "v":">"}, 
+                        "s": {"<":"<", ">":">", "^":"^", "v":"v"},
+                        "r": {"<":"^", ">":"v", "^":">", "v":"<"},
+                        }
+
+
+if __name__ == '__main__':
+    #Part 1
+    no_wags_rails, wagons = get_rails_wagons()
+    part1(wagons, no_wags_rails)
+    
+    print
+    
+    #Part 2
+    no_wags_rails, wagons = get_rails_wagons()
+    part2 (wagons, no_wags_rails)
 
